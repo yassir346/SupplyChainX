aff456d45299b01aa57db02afa7d67fea9f7bcc9
yassir346 <yassir.mahir98@gmail.com>
2025-11-11 09:35:03 +0100

    Insertion of service tester of product method (getAllProducts). As well, the test of bootstrap application.

---
+++ src/test/java/supplychainx/springboot/ProductRepositoryTests.java
@@ -0,0 +1,37 @@
+package supplychainx.springboot;
+
+import jakarta.persistence.Id;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import supplychainx.springboot.production.model.Product;
+import supplychainx.springboot.production.repository.ProductRepository;
+
+import static org.mockito.Mockito.when;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class ProductRepositoryTests {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Test
+    public void testCreateReadDelete(){
+        Product product = new Product();
+        product.setName("abc");
+        product.setCost(10);
+        product.setStock(1001);
+        product.setProductionTime(24);
+
+        productRepository.save(product);
+
+        Iterable<Product> products = productRepository.findAll();
+        Assertions.assertThat(products).extracting(Product::getName).containsOnlyOnce("abc");
+
+        productRepository.deleteById(11L);
+        Assertions.assertThat(productRepository.findAll().isEmpty());
+    }
+}
---
+++ src/test/java/supplychainx/springboot/ProductServiceTests.java
@@ -0,0 +1,81 @@
+package supplychainx.springboot;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import supplychainx.springboot.production.dto.ProductRequestDto;
+import supplychainx.springboot.production.model.Product;
+import supplychainx.springboot.production.repository.ProductRepository;
+import supplychainx.springboot.production.service.Impl.ProductServiceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ProductServiceTests {
+    @InjectMocks
+    ProductServiceImpl productService;
+
+    @Mock
+    ProductRepository productRepository;
+
+    @BeforeEach
+    public void init(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void findAllProductsTest(){
+        List<Product> list = new ArrayList<Product>();
+        Product product = new Product();
+        product.setId(1L);
+        product.setName("abc");
+        product.setCost(10);
+        product.setStock(1001);
+        product.setProductionTime(24);
+
+        Product product1 = new Product();
+        product1.setId(2L);
+        product1.setName("efg");
+        product1.setCost(11);
+        product1.setStock(100111);
+        product1.setProductionTime(244);
+
+        list.add(product);
+        list.add(product1);
+
+        when(productRepository.findAll()).thenReturn(list);
+
+        List<Product> productList = productService.findAllProducts();
+
+        Assertions.assertEquals(2, productList.size());
+        verify(productRepository, times(1)).findAll();
+     }
+
+     @Test
+    void testCreateProduct(){
+         ProductRequestDto productRequest = new ProductRequestDto();
+         productRequest.setName("newP");
+         productRequest.setCost(10);
+         productRequest.setStock(1001);
+         productRequest.setProductionTime(24);
+
+         Product product = new Product();
+         product.setId(3L);
+         product.setName("abc");
+         product.setCost(10);
+         product.setStock(1001);
+         product.setProductionTime(24);
+
+         when(productRepository.save(product)).thenReturn(product);
+         productService.create(productRequest);
+         verify(productRepository, times(1)).save(product);
+     }
+}
