8d2445b52f3647e78aaff9bde0f679f61911048e
yassir346 <yassir.mahir98@gmail.com>
2025-11-17 10:39:14 +0100

    Adding spring security configuration and dependencies in pom file

--- src/main/java/supplychainx/springboot/config/SecurityConfig.java
+++ src/main/java/supplychainx/springboot/config/SecurityConfig.java
@@ -1,8 +1,21 @@
 package supplychainx.springboot.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-    public
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/public/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+
+    }
 }
